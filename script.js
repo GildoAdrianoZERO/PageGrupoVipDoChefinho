b5beaaf9da89059f7505b0e3502eb447a9bf9571
GILDO ADRIANO NORBERTO DA SILVA <133284362+GildoAdrianoZERO@users.noreply.github.com>
2025-09-22 15:59:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ leadForm.addEventListener('submit', function(event) {
     const telefone = document.getElementById('telefone').value;
     
     // URL da API do Google Apps Script (certifique-se de que está correta)
-    const scriptUrl = 'https://script.google.com/macros/s/AKfycbxfDQsyVqYyEbnLf-oZkbwO8UUdUhUXKOK1RzPKdlWVYvbMDk18RS8z8e17CXgkzLMyzg/exec';
+    const scriptUrl = 'https://script.google.com/macros/s/AKfycby5Qq1OoPB0DS8-mMijBCpANLUcsKcxKPamRvbhVRv6CU5nXgbL4KBP-ORVh397cxVp/exec';
     
     // Monta o objeto FormData para enviar os dados
     const formData = new FormData();
